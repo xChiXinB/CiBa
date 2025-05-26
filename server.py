6801da819c3cf396c58fd30b19643a5cea9a2349
xChiXinB <zhizhongwang403@gmail.com>
2025-05-26 20:59:59 +0800

    为更多功能预备好；加入网站图标（暂定）

--- server.py
+++ server.py
@@ -14,24 +14,6 @@ flask_cors.CORS(app)
 
 @app.route('/vocab/<vocab>')
 def get_translation(vocab):
-    """# 合成网站
-    url = (f'https://www.bing.com/dict/{vocab}')
-
-    # Header
-    header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
-                  " Chrome/114.0.0.0 Safari/537.36",
-    }
-
-    # 爬取网站信息
-    response = requests.get(url, headers=header)
-
-    # 提取单词释义
-    bs = BeautifulSoup(response.text, 'html.parser')
-    # 处理no results
-    no_res = bs.find_all('div', class_ = 'no_results')
-
-    return translation"""
     # 初始化浏览器
     options = Options()
     options.add_argument('--user-agent=Mozilla/...Edge/136...')
@@ -64,8 +46,8 @@ def get_translation(vocab):
     edge_driver.quit()
     return response
 
-@app.route('/home')
+@app.route('/input')
 def home():
-    return flask.render_template("main.html")
+    return flask.render_template("input.html")
 
 app.run(debug=True, host='0.0.0.0', port=50907)
